9f50c2ad7e1efd9485a39d58351b6c93d72cc5a8
Asdow <20314541+Asdow@users.noreply.github.com>
2018-12-11 23:43:58 +0200

    Update obj.jl
    load vertex, normal and texture indices from file

--- src/obj.jl
+++ src/obj.jl
@@ -26,6 +26,9 @@ struct Mesh
     vertices::Vector{Vertex}
     normals::Vector{Normal}
     texcoords::Vector{TextureCoordinate}
+    vindices::Vector{SVec{3,UInt32}} # vertex indices
+    nindices::Vector{SVec{3,UInt32}} # normal indices
+    tindices::Vector{SVec{3,UInt32}} # texture indices
 end
 
 
@@ -35,6 +38,9 @@ function loadOBJ(path::String)
     vertices = Array{Vertex,1}[]
     normals = Array{Normal,1}[]
     texturecoords = Array{TextureCoordinate,1}[]
+    vertexFaces = Array{SVec{3,UInt32},1}[]
+    normalFaces = Array{SVec{3,UInt32},1}[]
+    textureFaces = Array{SVec{3,UInt32},1}[]
 
 
     stream = open(path)
@@ -51,13 +57,56 @@ function loadOBJ(path::String)
                 push!( vertices, Vertex[] )
                 push!( normals, Normal[] )
                 push!( texturecoords, TextureCoordinate[] )
-
+                push!( vertexFaces, SVec{3,UInt32}[] )
+                push!( normalFaces, SVec{3,UInt32}[] )
+                push!( textureFaces, SVec{3,UInt32}[] )
             elseif command == "v"
                 push!( vertices[end], parse.(Float32,lines) )
             elseif command == "vn"
                 push!( normals[end], parse.(Float32,lines) )
             elseif command == "vt"
                 push!( texturecoords[end], parse.(Float32,lines) )
+            # Handle faces
+        elseif command == "f"
+                # Handle missing texture coordinate indices case, ie.
+                # f v1//vn1 v2//vn2 v3//vn3 ...
+                if any( x->occursin("//", x), lines )
+                    indexes = split.(lines, "//")
+
+                    vertIndices = UInt32[]
+                    normalIndices = UInt32[]
+                    for i in 1:length(indexes)
+                        parsed = parse.(UInt32, indexes[i])
+                        push!(vertIndices, parsed[1])
+                        push!(normalIndices, parsed[2])
+                    end
+
+                    vertexTriangle = SVec3( vertIndices )
+                    push!( vertexFaces[end], vertexTriangle )
+                    normalTriangle = SVec3( normalIndices )
+                    push!( normalFaces[end], normalTriangle )
+                # Handle full indices case, ie.
+                # f v1/vt1/vn1 v2/vt2/vn2 v3/vt2/vn3 ...
+            elseif any(x->occursin("/", x), lines)
+                    indexes = split.(lines, "/")
+
+                    vertIndices = UInt32[]
+                    texCoordinateIndices =  UInt32[]
+                    normalIndices = UInt32[]
+                    for i in 1:length(indexes)
+                        parsed = parse.(UInt32, indexes[i])
+                        push!(vertIndices, parsed[1])
+                        push!(texCoordinateIndices, parsed[2])
+                        push!(normalIndices, parsed[3])
+                    end
+                    push!( vertexFaces[end],  SVec3( vertIndices ) )
+                    push!( normalFaces[end], SVec3( normalIndices ) )
+                    push!( textureFaces[end], SVec3( texCoordinateIndices ) )
+                # Handle only vertex indices
+                else
+                    parsed = parse.(UInt32, lines)
+                    push!( vertexFaces[end],  SVec3( parsed ) )
+                end
             end
         end
     end
@@ -66,7 +115,15 @@ function loadOBJ(path::String)
 
     meshes = Mesh[]
     for i in 1:length(names)
-        mesh = Mesh(names[i], vertices[i], normals[i], texturecoords[i])
+        mesh = Mesh(
+            names[i],
+            vertices[i],
+            normals[i],
+            texturecoords[i],
+            vertexFaces[i],
+            normalFaces[i],
+            textureFaces[i]
+        )
         push!( meshes, mesh )
     end
 
